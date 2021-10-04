0b52e6f1f40b68eee776741b2a8277505889be33
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-10-05 00:38:07 +0200

    ->packages-output

--- gorilla/util.scm
+++ gorilla/util.scm
@@ -4,13 +4,22 @@
   #:use-module (gnu packages)
   #:export (->manifests
             ->packages
+            ->packages-output
             ))
 
 (define (->packages specs)
     (map specification->package specs))
 
+(define (->packages-output specs)
+    (map specification->package+output specs))
+
+
+
+
 (define (->manifests specs)
     (packages->manifest (->packages specs)))
     
+    
+
     
     
