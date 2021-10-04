345766fa933063c9b1a203777357f1fedbe775ac
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-10-05 01:22:52 +0200

    test

--- gorilla/services/special-files.scm
+++ gorilla/services/special-files.scm
@@ -1,18 +1,11 @@
-(define-module (gorilla service special-files))
-
-(use-modules
- (srfi srfi-1)
- (gnu)
- (gnu system locale)
- (gnu services networking)
- (gnu services dbus)
- (gnu services desktop)
- (gnu services ssh)
- (gnu packages base)            ; for 'canonical-package'
- (gnu packages bash)
+(define-module (gorilla service special-files)
+   #:use-module (gnu)
+   #:use-module (gnu packages bash) 
+   #:export (service-special-files)
 )
 
-(define-public service-special-files
+
+(define service-special-files
    (service special-files-service-type
       `(("/bin/bash" ,(file-append bash "/bin/sh"))
         ("/bin/sh" ,(file-append bash "/bin/sh"))
