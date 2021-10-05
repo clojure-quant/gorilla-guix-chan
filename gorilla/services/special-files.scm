a415b105a877b8be33df506d7c4058f58d4d62d5
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-10-05 02:05:36 +0200

    service extra

--- gorilla/services/special-files.scm
+++ gorilla/services/special-files.scm
@@ -1,7 +1,8 @@
 (define-module (gorilla services special-files)
    #:use-module (gnu)
    #:use-module (gnu packages bash) 
-   #:export (service-special-files)
+   #:export (service-special-files
+             service-link-env )
 )
 
 
@@ -10,4 +11,8 @@
       `(("/bin/bash" ,(file-append bash "/bin/sh"))
         ("/bin/sh" ,(file-append bash "/bin/sh"))
         ("/usr/bin/env" ,(file-append coreutils "/bin/env")))))
-
+     
+     
+(define service-link-env     
+   (extra-special-file "/usr/bin/env"
+      (file-append coreutils "/bin/env")))
