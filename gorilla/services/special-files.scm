62533f86bc29b3a97e42d7144400b1b51b97a735
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-10-05 14:22:45 +0200

    bin-links

--- gorilla/services/special-files.scm
+++ gorilla/services/special-files.scm
@@ -1,18 +1,17 @@
 (define-module (gorilla services special-files)
    #:use-module (gnu)
    #:use-module (gnu packages bash) 
-   #:export (service-special-files
-             service-link-env )
-)
+   #:use-module (gnu packages fish) 
+   #:export (service-bin-links))
 
-
-(define service-special-files
+(define service-bin-links
    (service special-files-service-type
       `(("/bin/bash" ,(file-append bash "/bin/sh"))
         ("/bin/sh" ,(file-append bash "/bin/sh"))
+        ("/bin/fish" ,(file-append bash "/bin/fish"))
         ("/usr/bin/env" ,(file-append coreutils "/bin/env")))))
      
      
-(define service-link-env     
-   (extra-special-file "/usr/bin/env"
-      (file-append coreutils "/bin/env")))
+; (define service-bin-links
+;   (extra-special-file "/usr/bin/env"
+;      (file-append coreutils "/bin/env")))
