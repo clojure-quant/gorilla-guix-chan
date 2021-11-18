4805312ccfa3f0659f59607755f408e5b615ec3a
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-11-18 13:57:38 +0100

    clojure only from clojure-tools

--- gorilla/install/clojure.scm
+++ gorilla/install/clojure.scm
@@ -1,28 +1,28 @@
 (define-module (gorilla install clojure)
-  #:export (my-clojure
-            ))
+  #:export (my-clojure))
 
 (define my-clojure
   (list 
-  ;"icedtea"   ; open-jdk java
-        ;"openjdk"
-        ;(list "openjdk" "jdk" "doc" "out")
-        ;"openjdk:doc"  ; doc output of openjdk - needed by cider/orchard
-        "openjdk:jdk" ; having first openjdk does not work.s
-        "fastjar" ; java "jar" app
-        "java-qdox" 
-        "java-swt"
-        "clojure"   ; from gnu guix
-        "rlwrap"
-       
-        "graphviz" ;  dependency graphs
+    ;"icedtea"   ; open-jdk java
+    ;"openjdk"
+    ;(list "openjdk" "jdk" "doc" "out")
+    ;"openjdk:doc"  ; doc output of openjdk - needed by cider/orchard
+    "openjdk:jdk" ; having first openjdk does not work.s
+    "fastjar" ; java "jar" app
+    "java-qdox" 
+    "java-swt"
+    "rlwrap"
       
-       "node" ; v10, v14 is in development. brings node and npm
-       ; "java-certs" ; why?
+    ;"clojure-tools-cli"  ; guix version (OLD)
+    ;"clojure"   ; from gnu guix  this is old.  brings clojure
+    "clojure-tools" ; nongnu version brings clj and clojure
+    "leiningen" ; nongnu 
+    ; channel gorilla (prior: guiliani)
+    "babashka" 
+      
+    "node" ; v10, v14 is in development. brings node and npm
+    ; "java-certs" ; why?
+    "graphviz" ;  dependency graphs
 
-       ;"clojure-tools-cli"  ; guix version (OLD)
-       "clojure-tools" ; nongnu version
-       "leiningen" ; nongnu 
-        ; channel gorilla (prior: guiliani)
-       "babashka" 
+      
    ))
